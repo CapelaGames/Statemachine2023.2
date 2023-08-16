830e5461ef00295fd3e58637466034cd1a8dd84d
CapelaGames <andrew@Capelagames.com.au>
2023-08-16 15:57:56 +1000

    Squashed commit of the following:
    commit 0d19e78e02f0b58a830c61310d514108a22bb668
    Author: CapelaGames <andrew@Capelagames.com.au>
    Date:   Wed Aug 16 15:52:41 2023 +1000
    
        MonsterBattler
    
    commit bd1afa3a8ee357a0e4058f617c310acd89509bdb
    Author: CapelaGames <andrew@Capelagames.com.au>
    Date:   Thu Aug 10 16:59:00 2023 +1000
    
        wiggley boys

--- Assets/Statemachine/Scripts/AiStateMachine.cs
+++ Assets/Statemachine/Scripts/AiStateMachine.cs
@@ -1,21 +1,29 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR;
 
 public enum State
 {
-    Idle, Shoot, RunAway
+    Shoot, Run, Patrol, Wiggle
 }
 
 public class AiStateMachine : MonoBehaviour
 {
-    public State AiState = State.RunAway;
+    [SerializeField]
+    private State _aiState = State.Shoot;
+
+    [SerializeField]
+    private Transform _player;
 
     private SpriteRenderer _sprite;
 
-    public GameObject _player;
+    [SerializeField]
+    private Transform _eyes1;
+
+    [SerializeField]
+    private Transform _eyes2;
+
 
     void Start()
     {
@@ -25,43 +33,31 @@ public class AiStateMachine : MonoBehaviour
 
     private void NextState()
     {
-        switch (AiState)
+        switch (_aiState)
         {
-            case State.Idle:
-                StartCoroutine(IdleState());
-                break;
             case State.Shoot:
                 StartCoroutine(ShootState());
                 break;
-            case State.RunAway:
-                StartCoroutine(RunAwayState());
+            case State.Run:
+                StartCoroutine(RunState());
+                break;
+            case State.Patrol:
+                StartCoroutine(PatrolState());
+                break;
+            case State.Wiggle:
+                StartCoroutine(WiggleState());
+                break;
+            default:
+                Debug.LogError("");
                 break;
         }
-    }
-    private IEnumerator IdleState()
-    {
-        Debug.Log("Entering Idle State");
-        while (AiState == State.Idle)
-        {
-            _sprite.transform.rotation *= Quaternion.Euler(0, 0, 50f * Time.deltaTime);
-
-            Vector2 directionToPlayer = _player.transform.position - gameObject.transform.position;
-                
-            if (Vector2.Angle(gameObject.transform.right, directionToPlayer) < 5f)
-            {
-                AiState = State.RunAway;
-            }
-            yield return null;
-        }
-        Debug.Log("Ending Idle State");
-        NextState();
     }
 
     private IEnumerator ShootState()
     {
-        Debug.Log("Entering Shoot State");
+        Debug.Log("Start Shoot");
         int colorChangeCount = 0;
-        while(AiState == State.Shoot)
+        while (_aiState == State.Shoot)
         {
             yield return new WaitForSeconds(0.5f);
             _sprite.color = new Color(
@@ -70,38 +66,89 @@ public class AiStateMachine : MonoBehaviour
                 Random.Range(0f, 1f));
             colorChangeCount++;
 
-            if(colorChangeCount == 5)
+            if (colorChangeCount >= 5)
             {
-                AiState = State.Idle;
+                _aiState = State.Patrol;
             }
+
             yield return null;
         }
-        Debug.Log("Ending Shoot State");
+        Debug.Log("End Shoot");
         NextState();
     }
 
-    private IEnumerator RunAwayState()
+    private IEnumerator WiggleState()
     {
-        Debug.Log("Entering RunAway State");
         float startTime = Time.time;
-        while (AiState == State.RunAway)
+        Vector3 e1 = _eyes1.transform.localScale;
+        Vector3 e2 = _eyes2.transform.localScale;
+        while (_aiState == State.Wiggle)
+        {
+            float wave = Mathf.Sin(Time.time * 30f) * 0.05f;
+            float wave2 = Mathf.Cos(Time.time * 30f  + 0.5f) * 0.05f;
+
+            _eyes1.transform.localScale += new Vector3(wave2, wave, 1) * 0.05f;
+            _eyes2.transform.localScale += new Vector3(wave, wave2, 1) * 0.05f;
+
+            if (Time.time - startTime > 2f)
+            {
+                _aiState = State.Run;
+            }
+
+            yield return null;
+        }
+        _eyes1.transform.localScale = e1;
+        _eyes2.transform.localScale = e2;
+        NextState();
+    }
+
+    private IEnumerator RunState()
+    {
+        Debug.Log("Start Run");
+        float startTime = Time.time;
+        while (_aiState == State.Run)
         {
             float wave = Mathf.Sin(Time.time * 30f) * 0.1f + 1f;
             float wave2 = Mathf.Cos(Time.time * 30f) * 0.1f + 1f;
-            transform.localScale = new Vector3 (wave2, wave,1);
 
-            float moveWave = Mathf.Sin(Time.time * 30f) * 0.9f + 0.3f;
-            transform.position += transform.right * moveWave * 5f * Time.deltaTime;
+            transform.localScale = new Vector3(wave2, wave, 1);
 
-            if ((Time.time - startTime) > 3f)
+            float shimmy = Mathf.Sin(Time.time * 30f) * 0.9f + 0.3f;
+            transform.position += transform.right * shimmy * Time.deltaTime;
+
+            if (Time.time - startTime > 3f)
             {
-                AiState = State.Shoot;
+                _aiState = State.Shoot;
+            }
+            yield return null;
+        }
+        transform.localScale = Vector3.one;
+        Debug.Log("End Run");
+        NextState();
+    }
+
+
+    private IEnumerator PatrolState()
+    {
+        Debug.Log("Start Paw Patrol");
+
+
+        while (_aiState == State.Patrol)
+        {
+            transform.rotation *= Quaternion.Euler(0f, 0f, 50f * Time.deltaTime);
+
+            // FROM A to B  -----   B - A
+            Vector2 directionToPlayer = _player.position - transform.position;
+
+            if (Vector2.Angle(transform.right, directionToPlayer) < 5f)
+            {
+                _aiState = State.Wiggle;
             }
 
             yield return null;
         }
-        Debug.Log("Ending RunAway State");
+        Debug.Log("End Patrol");
         NextState();
     }
-}
 
+}
